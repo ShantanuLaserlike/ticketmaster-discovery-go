368d091c782a70e6d13bdc5b0ada4c9b8dd95465
ShantanuLaserlike <shantanu@laserlike.com>
2018-06-13 14:11:41 -0700

    Updating the Events struct

--- disco/models.go
+++ disco/models.go
@@ -84,7 +84,7 @@ type Event struct {
 	Name            string           `json:"name"`
 	Type            string           `json:"type"`
 	Id              string           `json:"id"`
-	Test            bool           `json:"test"`
+	Test            bool             `json:"test"`
 	Url             string           `json:"url"`
 	Local           string           `json:"locale"`
 	Images          []Image          `json:"images"`
